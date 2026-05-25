c4af4b65d670030d86ad965228ec94db22452f10
James Smith <jismith1989@googlemail.com>
2026-05-25 15:03:20 +0100

    Move get-all-source Powershell file into util directory

---
+++ util/get-all-src.ps1
@@ -0,0 +1,40 @@
+$includeExtensions = @(
+	'.go'
+)
+
+$includeFileNames = @()
+
+$excludeDirPattern = '([\\/])(\.git|dist)([\\/]|$)'
+
+$excludeNestedDirs = @()
+
+$excludeNestedDirPatterns = $excludeNestedDirs | ForEach-Object {
+	'([\\/])' + (($_ | ForEach-Object { [regex]::Escape($_) }) -join '[\\/]') + '([\\/]|$)'
+}
+
+$excludeFileNames = @()
+
+Get-ChildItem -Recurse -File |
+	Where-Object {
+		$fullName = $_.FullName
+		$extension = $_.Extension.ToLowerInvariant()
+		$name = $_.Name
+
+		$fullName -notmatch $excludeDirPattern -and
+		-not ($excludeNestedDirPatterns | Where-Object { $fullName -match $_ }) -and
+		$excludeFileNames -notcontains $name -and
+		(
+			$includeExtensions -contains $extension -or
+			$includeFileNames -contains $name
+		)
+	} |
+	Sort-Object FullName |
+	ForEach-Object {
+		$relativePath = Resolve-Path -Relative $_.FullName
+
+		""
+		"===== $relativePath ====="
+		""
+		Get-Content $_.FullName -Encoding utf8
+	} |
+	Set-Content all-src.txt -Encoding utf8
