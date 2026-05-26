4f1402263506e884e37620b7eb472a21aefb216e
James Smith <jismith1989@googlemail.com>
2026-05-26 12:47:23 +0100

    Add CHARACTERS function to std_types_string

--- src/std_types_string.go
+++ src/std_types_string.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func NewStdTypesStringMap() Value {
 	entries := map[string]Binding{
-		"NEW": NewImmutableBinding(NewBuiltinFunctionValue(stdTypesStringNew)),
+		"CHARACTERS": NewImmutableBinding(NewBuiltinFunctionValue(stdTypesStringCharacters)),
+		"NEW":        NewImmutableBinding(NewBuiltinFunctionValue(stdTypesStringNew)),
 	}
 
 	return NewMapValue(entries, true)
@@ -37,5 +38,41 @@ func stdTypesStringNew(_ *Interpreter, args []Value) (Value, error) {
 
 	default:
 		return Value{}, fmt.Errorf("TYPES.STRING.NEW cannot convert unknown value kind")
+	}
+}
+
+func stdTypesStringCharacters(_ *Interpreter, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return Value{}, fmt.Errorf("TYPES.STRING.CHARACTERS expected 1 argument, got %d", len(args))
+	}
+
+	value := resolveSpecializedValue(args[0])
+
+	switch value.Kind {
+	case ValueString:
+		if value.Text == nil {
+			return NewVoidValue(), nil
+		}
+
+		elements := make([]Value, 0, len(value.Text.Runes))
+
+		for _, ch := range value.Text.Runes {
+			elements = append(elements, NewStringValue(string(ch)))
+		}
+
+		return NewArrayValue(elements, false), nil
+
+	case ValueVoid,
+		ValueEmptyCollection,
+		ValueNumber,
+		ValueBool,
+		ValueMap,
+		ValueArray,
+		ValueFunction,
+		ValueBuiltinFunction:
+		return NewVoidValue(), nil
+
+	default:
+		return Value{}, fmt.Errorf("TYPES.STRING.CHARACTERS cannot inspect unknown value kind")
 	}
 }
