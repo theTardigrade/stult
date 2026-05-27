e21709c2db88886ecad2ecaed1dc36291e016f79
James Smith <jismith1989@googlemail.com>
2026-05-27 17:46:46 +0100

    Move tokens out of lexer into own file

---
+++ src/token.go
@@ -0,0 +1,58 @@
+package main
+
+type TokenType string
+
+const (
+	TokenIllegal TokenType = "ILL"
+	TokenEOF     TokenType = "EOF"
+	TokenNewline TokenType = "NL"
+
+	TokenIdentifier TokenType = "ID"
+	TokenNumber     TokenType = "NUM"
+	TokenString     TokenType = "STR"
+	TokenBool       TokenType = "BOOL"
+
+	TokenPlusAssign  TokenType = ":+"
+	TokenMinusAssign TokenType = ":-"
+	TokenComma       TokenType = ","
+	TokenColon       TokenType = ":"
+	TokenAt          TokenType = "@"
+	TokenCaret       TokenType = "^"
+
+	TokenRangeInclusive TokenType = ".."
+	TokenRangeExclusive TokenType = "..."
+
+	TokenPlus  TokenType = "+"
+	TokenMinus TokenType = "-"
+	TokenStar  TokenType = "*"
+	TokenSlash TokenType = "/"
+
+	TokenAnd TokenType = "&"
+	TokenOr  TokenType = "|"
+
+	TokenLParen TokenType = "("
+	TokenRParen TokenType = ")"
+
+	TokenLBracket TokenType = "["
+	TokenRBracket TokenType = "]"
+
+	TokenLBrace TokenType = "{"
+	TokenRBrace TokenType = "}"
+
+	TokenEqual        TokenType = "="
+	TokenNotEqual     TokenType = "!"
+	TokenLess         TokenType = "<"
+	TokenLessEqual    TokenType = "<="
+	TokenGreater      TokenType = ">"
+	TokenGreaterEqual TokenType = ">="
+)
+
+type Token struct {
+	Type          TokenType
+	Literal       string
+	StartOfLine   int
+	StartOfColumn int
+	EndOfLine     int
+	EndOfColumn   int
+	IsImmutable   bool // true for all-uppercase identifiers such as PI or MAX_SIZE
+}
