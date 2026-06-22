b552a2902f38e8eb1088a19bca1b9b64a62e75cf
James Smith <jismith1989@googlemail.com>
2026-06-22 22:34:05 +0100

    Create ENTRIES function in std_type_map

--- src/std_type_map.go
+++ src/std_type_map.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func NewStdTypeMapMap() Value {
 	entries := map[string]Binding{
-		"KEYS":   NewImmutableBinding(NewBuiltinFunctionValue(StdTypeMapKeys)),
-		"VALUES": NewImmutableBinding(NewBuiltinFunctionValue(StdTypeMapValues)),
+		"ENTRIES": NewImmutableBinding(NewBuiltinFunctionValue(StdTypeMapEntries)),
+		"KEYS":    NewImmutableBinding(NewBuiltinFunctionValue(StdTypeMapKeys)),
+		"VALUES":  NewImmutableBinding(NewBuiltinFunctionValue(StdTypeMapValues)),
 	}
 
 	return NewMapValue(entries, true)
@@ -44,6 +45,47 @@ func StdTypeMapKeys(_ *RuntimeContext, args []Value) (Value, error) {
 
 	default:
 		return Value{}, fmt.Errorf("TYPE.MAP.KEYS cannot inspect unknown value kind")
+	}
+}
+
+func StdTypeMapEntries(_ *RuntimeContext, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return Value{}, fmt.Errorf("TYPE.MAP.ENTRIES expected 1 argument, got %d", len(args))
+	}
+
+	value := resolveSpecializedValue(args[0])
+
+	switch value.Kind {
+	case ValueMap:
+		if value.Map == nil {
+			return Value{}, fmt.Errorf("TYPE.MAP.ENTRIES cannot inspect invalid map")
+		}
+
+		keys := sortedMapKeys(value.Map)
+		elements := make([]Value, 0, len(keys))
+
+		for _, key := range keys {
+			pair := NewArrayValue([]Value{
+				NewStringValue(key),
+				value.Map.Entries[key].Value,
+			}, false)
+
+			elements = append(elements, pair)
+		}
+
+		return NewArrayValue(elements, false), nil
+
+	case ValueVoid,
+		ValueNumber,
+		ValueBool,
+		ValueString,
+		ValueArray,
+		ValueFunction,
+		ValueBuiltinFunction:
+		return NewVoidValue(), nil
+
+	default:
+		return Value{}, fmt.Errorf("TYPE.MAP.ENTRIES cannot inspect unknown value kind")
 	}
 }
 
