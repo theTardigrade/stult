d439b4c58d20045d2088e213eb8d5cee047d247b
James Smith <jismith1989@googlemail.com>
2026-06-23 21:14:28 +0100

    Update NUMBER.FORMAT function

--- src/std_type_number.go
+++ src/std_type_number.go
@@ -71,8 +71,8 @@ func StdTypeNumberClamp(_ *RuntimeContext, args []Value) (Value, error) {
 }
 
 func StdTypeNumberFormat(_ *RuntimeContext, args []Value) (Value, error) {
-	if len(args) != 2 {
-		return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT expected 2 arguments, got %d", len(args))
+	if len(args) < 1 || len(args) > 3 {
+		return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT expected 1 to 3 arguments, got %d", len(args))
 	}
 
 	value := resolveSpecializedValue(args[0])
@@ -80,16 +80,147 @@ func StdTypeNumberFormat(_ *RuntimeContext, args []Value) (Value, error) {
 		return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT argument 1 expected a number")
 	}
 
-	decimalPlaces, err := numberToInt64(args[1], "TYPE.NUMBER.FORMAT argument 2")
-	if err != nil {
-		return Value{}, err
+	decimalPlaces := int64(DefaultDecimalPlacesToDisplay)
+	options := numberFormatOptions{}
+
+	if len(args) >= 2 {
+		second := resolveSpecializedValue(args[1])
+
+		switch second.Kind {
+		case ValueNumber:
+			parsedDecimalPlaces, err := numberToInt64(second, "TYPE.NUMBER.FORMAT argument 2")
+			if err != nil {
+				return Value{}, err
+			}
+
+			decimalPlaces = parsedDecimalPlaces
+
+		case ValueMap:
+			if len(args) == 3 {
+				return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT argument 2 expected a number when argument 3 is supplied")
+			}
+
+			parsedOptions, err := parseNumberFormatOptions(second, "TYPE.NUMBER.FORMAT argument 2")
+			if err != nil {
+				return Value{}, err
+			}
+
+			options = parsedOptions
+
+		default:
+			return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT argument 2 expected a number or map")
+		}
+	}
+
+	if len(args) == 3 {
+		third := resolveSpecializedValue(args[2])
+		if third.Kind != ValueMap {
+			return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT argument 3 expected a map")
+		}
+
+		parsedOptions, err := parseNumberFormatOptions(third, "TYPE.NUMBER.FORMAT argument 3")
+		if err != nil {
+			return Value{}, err
+		}
+
+		options = parsedOptions
 	}
 
 	if decimalPlaces < 0 || decimalPlaces > MaxDecimalPlaces {
 		return Value{}, fmt.Errorf("TYPE.NUMBER.FORMAT decimal places must be between 0 and %d", MaxDecimalPlaces)
 	}
 
-	return NewStringValue(value.Number.Format(int(decimalPlaces))), nil
+	number := value.Number
+	if options.Percent {
+		number = numberMultiply(number, NewSmallNumber(100))
+	}
+
+	text := number.Format(int(decimalPlaces))
+	if options.GroupDigits {
+		text = groupFormattedNumberDigits(text)
+	}
+
+	if options.Percent {
+		text += "%"
+	}
+
+	return NewStringValue(text), nil
+}
+
+type numberFormatOptions struct {
+	Percent     bool
+	GroupDigits bool
+}
+
+func parseNumberFormatOptions(value Value, argumentName string) (numberFormatOptions, error) {
+	value = resolveSpecializedValue(value)
+	if value.Kind != ValueMap || value.Map == nil {
+		return numberFormatOptions{}, fmt.Errorf("%s expected a map", argumentName)
+	}
+
+	options := numberFormatOptions{}
+
+	for key, binding := range value.Map.Entries {
+		optionValue := resolveSpecializedValue(binding.Value)
+		if optionValue.Kind != ValueBool {
+			return numberFormatOptions{}, fmt.Errorf("TYPE.NUMBER.FORMAT option %q expected a bool", key)
+		}
+
+		switch key {
+		case "PERCENT":
+			options.Percent = optionValue.Bool
+
+		case "GROUP_DIGITS":
+			options.GroupDigits = optionValue.Bool
+
+		default:
+			return numberFormatOptions{}, fmt.Errorf("TYPE.NUMBER.FORMAT unknown option %q", key)
+		}
+	}
+
+	return options, nil
+}
+
+func groupFormattedNumberDigits(text string) string {
+	if text == "" {
+		return text
+	}
+
+	sign := ""
+	if strings.HasPrefix(text, "-") {
+		sign = "-"
+		text = strings.TrimPrefix(text, "-")
+	}
+
+	whole := text
+	fraction := ""
+
+	if dot := strings.Index(text, "."); dot >= 0 {
+		whole = text[:dot]
+		fraction = text[dot:]
+	}
+
+	if len(whole) <= 3 {
+		return sign + whole + fraction
+	}
+
+	firstGroupLength := len(whole) % 3
+	if firstGroupLength == 0 {
+		firstGroupLength = 3
+	}
+
+	var builder strings.Builder
+	builder.WriteString(sign)
+	builder.WriteString(whole[:firstGroupLength])
+
+	for i := firstGroupLength; i < len(whole); i += 3 {
+		builder.WriteString("'")
+		builder.WriteString(whole[i : i+3])
+	}
+
+	builder.WriteString(fraction)
+
+	return builder.String()
 }
 
 func StdTypeNumberFormatScientific(_ *RuntimeContext, args []Value) (Value, error) {
